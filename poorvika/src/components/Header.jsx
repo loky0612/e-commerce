8f23b6a9baf140c882cc26b9d5b0490d941e67e0
loky0612 <lokeshmalairaj@gmail.com>
2023-06-11 22:51:49 +0530

    commit

--- poorvika/src/components/Header.jsx
+++ poorvika/src/components/Header.jsx
@@ -1,29 +1,21 @@
 import React from 'react';
 import './Header.css';
-import logo from '../Images/Poorvika.png';
-import Banners from './Banners';
 
 const Header = () => {
   return (
     <div className='header'>
-        <div className='container flex'>
-            <div className='logo'>
-                <img src={logo} alt='logo'/>
-            </div>
-            <div className='searchbar'>
-                <i class="fa fa-search icon"></i>
-                <input type="text" placeholder='Search Products, Brands, Offers' />
-            </div>
-            <div className='nav'>
-                <ul>
-                    <li>Delivery</li>
-                    <li>Locate</li>
-                    <li>Items</li>
-                    <li>My account</li>
-                </ul>
-            </div>
-        </div>
-        <Banners/>
+    <header class="header">
+        <a href="/" class="logo">lr</a>
+        <input class="side-menu" type="checkbox" id="side-menu"/>
+        <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
+        <nav class="nav">
+            <ul class="menu">
+                <li><a href="/">Gallery</a></li>
+                <li><a href="/">Blog</a> </li>
+                <li><a href="/">About</a></li>
+            </ul>
+        </nav>
+    </header>
     </div>
   )
 }
